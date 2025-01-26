52b0abb130cf82bcd57bdcac7af566c5df5e6784
Teramura Takumi <tyc.soccer0602@gmail.com>
2025-01-26 06:53:25 +0000

    A.blade.phpを追加

---
+++ resources/views/A.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=egde">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token()}}">
+
+    <title>A</title>
+
+
+    <link rel="dns-prefetch" href="http://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<p>AAA</p>
+<p>BBB</p>
+<p>CCC</p>
+</body>
